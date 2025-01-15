4d7eed8068139eee3ed04cf043434d467b37dc9c
Windows <taherehhassani@outlook.com>
2025-01-15 14:57:52 +0100

    Add testFile

--- src/TextCounter.java
+++ src/TextCounter.java
@@ -1,15 +1,20 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class TextCounter {
+    private int numberOfChar = 0;
+    private int numberOfRow = 0;
+
+    public void count(String input){
+        if(input!=null && !input.equalsIgnoreCase("stop")){
+            numberOfChar += input.length(); // lägg till teckenlängden för raden
+            numberOfRow++; // öka antalet rader
         }
     }
+    public int getNumberOfChar(){
+        return  numberOfChar;
+
+    }
+    public int getNumberOfRow(){
+        return numberOfRow;
+    }
+
 }
